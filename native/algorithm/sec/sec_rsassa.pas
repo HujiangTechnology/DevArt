15401edb5659fe3c9a4fab07a21742b430776773
rarnu <rarnu1985@gmail.com>
2016-04-05 11:40:57 +0800

    * change param seq

--- native/algorithm/sec/sec_rsassa.pas
+++ native/algorithm/sec/sec_rsassa.pas
@@ -19,17 +19,17 @@ type
     procedure evtGetSignatre(Sender: TObject; var Sig: TRSASignatureBlock);
   end;
 
-function _rsassaGenerateKeys(keySize: Integer; pubPass: PChar; pubPath: PChar; privPass: PChar; privPath: PChar): Integer; stdcall;
+function _rsassaGenerateKeys(keySize: Integer; pubPass: PChar; privPass: PChar; pubSavePath: PChar; privSavePath: PChar): Integer; stdcall;
 function _rsassaSignString(keySize: Integer; hashMethod: Integer; privPass: PChar; privPath: PChar; str: PChar): PChar; stdcall;
 function _rsassaSignFile(keySize: Integer; hashMethod: Integer; privPass: PChar; privPath: PChar; filePath: PChar): PChar; stdcall;
 function _rsassaVerifyString(keySize: Integer; hashMethod: Integer; pubPass: PChar; pubPath: PChar; sig: PChar; str: PChar): Integer; stdcall;
 function _rsassaVerifyFile(keySize: Integer; hashMethod: Integer; pubPass: PChar; pubPath: PChar; sig: PChar; filePath: PChar): Integer; stdcall;
-function rsassaGenerateKeys(keySize: Integer; pubPass: PChar; pubPath: PChar; privPass: PChar; privPath: PChar): Integer; stdcall;
+function rsassaGenerateKeys(keySize: Integer; pubPass: PChar; privPass: PChar; pubSavePath: PChar; privSavePath: PChar): Integer; stdcall;
 function rsassaSignString(keySize: Integer; hashMethod: Integer; privPass: PChar; privPath: PChar; str: PChar): PChar; stdcall;
 function rsassaSignFile(keySize: Integer; hashMethod: Integer; privPass: PChar; privPath: PChar; filePath: PChar): PChar; stdcall;
 function rsassaVerifyString(keySize: Integer; hashMethod: Integer; pubPass: PChar; pubPath: PChar; sig: PChar; str: PChar): Integer; stdcall;
 function rsassaVerifyFile(keySize: Integer; hashMethod: Integer; pubPass: PChar; pubPath: PChar; sig: PChar; filePath: PChar): Integer; stdcall;
-function Java_com_hujiang_devart_security_AlgorithmUtils_rsassaGenerateKeys(env: PJNIEnv; obj: jobject; keySize: jint; pubPass: jstring; pubPath: jstring; privPass: jstring; privPath: jstring): jint; stdcall;
+function Java_com_hujiang_devart_security_AlgorithmUtils_rsassaGenerateKeys(env: PJNIEnv; obj: jobject; keySize: jint; pubPass: jstring; privPass: jstring; pubSavePath: jstring;  privSavePath: jstring): jint; stdcall;
 function Java_com_hujiang_devart_security_AlgorithmUtils_rsassaSignString(env: PJNIEnv; obj: jobject; keySize: jint; hashMethod: jint; privPass: jstring; privPath: jstring; str: jstring): jstring; stdcall;
 function Java_com_hujiang_devart_security_AlgorithmUtils_rsassaSignFile(env: PJNIEnv; obj: jobject; keySize: jint; hashMethod: jint; privPass: jstring; privPath: jstring; filePath: jstring): jstring; stdcall;
 function Java_com_hujiang_devart_security_AlgorithmUtils_rsassaVerifyString(env: PJNIEnv; obj: jobject; keySize: jint; hashMethod: jint; pubPass: jstring; pubPath: jstring; sig: jstring; str: jstring): jint; stdcall;
@@ -37,8 +37,8 @@ function Java_com_hujiang_devart_security_AlgorithmUtils_rsassaVerifyFile(env: P
 
 implementation
 
-function _rsassaGenerateKeys(keySize: Integer; pubPass: PChar; pubPath: PChar;
-  privPass: PChar; privPath: PChar): Integer; stdcall;
+function _rsassaGenerateKeys(keySize: Integer; pubPass: PChar; privPass: PChar;
+  pubSavePath: PChar; privSavePath: PChar): Integer; stdcall;
 var
   rsassa: TLbRSASSA;
 begin
@@ -49,9 +49,9 @@ begin
     rsassa.KeySize:= TLbAsymKeySize(keySize);
     rsassa.GenerateKeyPair;
     rsassa.PublicKey.Passphrase:= string(pubPass);
-    rsassa.PublicKey.StoreToFile(string(pubPath));
+    rsassa.PublicKey.StoreToFile(string(pubSavePath));
     rsassa.PrivateKey.Passphrase:= string(privPass);
-    rsassa.PrivateKey.StoreToFile(string(privPass));
+    rsassa.PrivateKey.StoreToFile(string(privSavePath));
     Result := 0;
   finally
     rsassa.Free;
@@ -153,10 +153,10 @@ begin
   end;
 end;
 
-function rsassaGenerateKeys(keySize: Integer; pubPass: PChar; pubPath: PChar;
-  privPass: PChar; privPath: PChar): Integer; stdcall;
+function rsassaGenerateKeys(keySize: Integer; pubPass: PChar; privPass: PChar;
+  pubSavePath: PChar; privSavePath: PChar): Integer; stdcall;
 begin
-  Result := _rsassaGenerateKeys(keySize, pubPass, pubPath, privPass, privPath);
+  Result := _rsassaGenerateKeys(keySize, pubPass, privPass, pubSavePath,privSavePath);
 end;
 
 function rsassaSignString(keySize: Integer; hashMethod: Integer;
@@ -186,9 +186,10 @@ end;
 
 function Java_com_hujiang_devart_security_AlgorithmUtils_rsassaGenerateKeys(
   env: PJNIEnv; obj: jobject; keySize: jint; pubPass: jstring;
-  pubPath: jstring; privPass: jstring; privPath: jstring): jint; stdcall;
+  privPass: jstring; pubSavePath: jstring; privSavePath: jstring): jint;
+  stdcall;
 begin
-  Result := _rsassaGenerateKeys(keySize, PChar(jstringToString(env, pubPass)), PChar(jstringToString(env, pubPath)), PChar(jstringToString(env, privPass)), PChar(jstringToString(env, privPath)));
+  Result := _rsassaGenerateKeys(keySize, PChar(jstringToString(env, pubPass)), PChar(jstringToString(env, privPass)), PChar(jstringToString(env, pubSavePath)), PChar(jstringToString(env, privSavePath)));
 end;
 
 function Java_com_hujiang_devart_security_AlgorithmUtils_rsassaSignString(
